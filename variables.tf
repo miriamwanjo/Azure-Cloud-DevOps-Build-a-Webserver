0284c9a6111303a38461a02d898e01acf94cfe8e
Miriam Wanjohi <njoks00@yahoo.com>
2021-03-07 12:33:24 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "prefix" {
   description = "The prefix which should be used for all resources in this example"
-  default      = "mw-udacity-webserver2"
+  default      = "mw-webserver1"
 }
 
 variable "location" {
